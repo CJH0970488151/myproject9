adf26f53b76fcfd49ae18351ffc763d59474d1fa
邱勁豪 <a145621308@gmail.com>
2022-06-17 16:37:50 +0800

    Student with Kotlin

---
+++ src/main/java/com/tom/score/Student.kt
@@ -0,0 +1,17 @@
+package com.tom.com.tom.score
+
+class Student (var name:String,var english:Int,var math:Int){
+    init {
+        println()
+    }
+    fun print(){
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage() = (english+math)/2
+
+}
+
+fun main() {
+    val stu = Student("Jack",55,87)
+    stu.print()
+}
